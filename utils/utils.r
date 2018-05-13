b0136df6aae282293a489e77f6c9cbc9535365df
Michael Beigelmacher <brooklynbagel@gmail.com>
2018-05-13 00:21:53 -0400

    Remove usage of deprecated package tidyjson from data preparation

--- utils/utils.r
+++ utils/utils.r
@@ -1,7 +1,10 @@
 library(crosstalk)
 library(magrittr)
-library(tidyjson)
 library(htmltools)
+library(tibble)
+library(dplyr)
+library(tidyr)
+library(purrr)
 source('utils/JsonToDataFrame.r')
 source('utils/DataTableBuilder.r')
 # for functions to build a filter with complex key
@@ -9,9 +12,9 @@ source('utils/ComplexFilter.r')
 # for building complex keys
 source('utils/KeyBuilding.r')
 
-# meetupInfo <- jsonlite::fromJSON('data/events.json') %>% 
-#     makeInfoDF() %>% 
-#     narrowDownInfo() %>% 
+# meetupInfo <- jsonlite::fromJSON('data/events.json') %>%
+#     makeInfoDF() %>%
+#     narrowDownInfo() %>%
 #     renameInfo()
 
 # create shared data
@@ -22,7 +25,7 @@ source('utils/KeyBuilding.r')
 # makeDatatable(meetupInfo[1:5, 1:3])
 
 # read event data
-eventData <- tidyjson::read_json('data/events.json')
+eventData <- jsonlite::fromJSON("data/events.json", simplifyVector = FALSE, flatten = TRUE)
 
 # get speaker info
 # venue info
@@ -30,15 +33,15 @@ eventData <- tidyjson::read_json('data/events.json')
 
 stripListChars <- function(data)
 {
-    data %>% 
-        dplyr::mutate_if(is.character, stringr::str_replace_all, pattern='^list\\(\\"', replace='') %>% 
+    data %>%
+        dplyr::mutate_if(is.character, stringr::str_replace_all, pattern='^list\\(\\"', replace='') %>%
         dplyr::mutate_if(is.character, stringr::str_replace_all, pattern='\\"\\)$', replace='')
 }
 
 stripListCharsFromDate <- function(data)
 {
-    data %>% 
-        dplyr::mutate_at(dplyr::vars(Date), stringr::str_replace_all, pattern='^list\\(', replace='') %>% 
+    data %>%
+        dplyr::mutate_at(dplyr::vars(Date), stringr::str_replace_all, pattern='^list\\(', replace='') %>%
         dplyr::mutate_at(dplyr::vars(Date), stringr::str_replace_all, pattern='\\)$', replace='')
 }
 
@@ -52,92 +55,93 @@ makeCharVector <- function(x)
     list(unique(x))
 }
 
-# general info
-generalInfo <- eventData %>% 
-    gather_array %>% 
-    # get these details
-    spread_values(ID=jstring('id'), 
-                  URL=jstring('link'), 
-                  Date=jstring('time'),
-                  Meetup=jstring('name'),
-                  Venue=jstring('venue', 'name'),
-                  Description=jstring('description'),
-                  Video=jstring('video'),
-                  VideoSource=jstring('VideoSource')
-    ) %>% 
-    stripListChars %>% 
-    stripListCharsFromDate %>% 
-    # convert to data
-    dplyr::mutate(Date=as.Date(as.POSIXct(as.numeric(Date)/1000, origin='1970-01-01 00:00:00')))
+# general info ----
+generalInfo <- data_frame(
+  ID = eventData %>% map("id") %>% unlist(),
+  URL = eventData %>% map("link") %>% unlist(),
+  Date = eventData %>% map("time") %>% unlist(),
+  Meetup = eventData %>% map("name") %>% unlist(),
+  Venue = eventData %>% map("venue") %>% map("name") %>% unlist(),
+  Description = eventData %>% map("description") %>% unlist(),
+  Video = eventData %>% map("video") %>% map(~ .x %||% list("")) %>% unlist(),
+  VideoSource = eventData %>% map("VideoSource") %>% map(~ .x %||% list("")) %>% unlist()
+) %>%
+  mutate(Date = Date %>%
+           magrittr::divide_by(1000) %>%
+           as.POSIXct(tz = "America/New_York", origin = lubridate::origin) %>%
+           as.Date())
+generalInfo
+
 
 # speakers
-speakerInfo <- eventData %>% 
-    gather_array %>% 
-    spread_values(ID=jstring('id')) %>% 
-    enter_object('speakers') %>% 
-    gather_array %>% append_values_string(column.name='Speaker') %>% 
-    stripListChars %>% 
-    ditchCols
-    
-# topics
-topicInfo <- eventData %>% 
-    gather_array %>% 
-    spread_values(ID=jstring('id')) %>% 
-    enter_object('topics') %>% 
-    gather_array %>% append_values_string(column.name='Topic') %>% 
-    stripListChars %>% 
-    ditchCols
+speakerInfo <- data_frame(
+  ID = eventData %>% map("id") %>% unlist(),
+  Speaker = eventData %>% map("speakers") %>% map(~ .x %||% list("") %>% unlist())
+) %>%
+  unnest()
+speakerInfo
 
-presentationInfo <- eventData %>% 
-    gather_array %>% 
-    spread_values(ID=jstring('id')) %>% 
-    enter_object('slides') %>% 
-    gather_array %>% 
-    spread_values(Presenter=jstring('presenter'), 
-                  Title=jstring("title"), 
-                  File=jstring("file"), 
-                  Format=jstring("format")
-    ) %>% 
-    enter_object('topics') %>% 
-    gather_array %>% append_values_string(column.name='PresentationTopic') %>% 
-    stripListChars %>% 
-    ditchCols
+
+# topics ----
+topicInfo <- data_frame(
+  ID = eventData %>% map("id") %>% unlist(),
+  Topic = eventData %>% map("topics") %>% map(~ .x %||% list("")) %>% map(unlist)
+) %>%
+  unnest()
+topicInfo
+
+
+# presentations ----
+presentationInfo <- data_frame(
+  ID = eventData %>% map("id") %>% unlist(),
+  Slides = eventData %>% map("slides") %>% map(~ .x %||% list())
+) %>%
+  unnest() %>%
+  mutate(Presenter = Slides %>% map("presenter"),
+         Title = Slides %>% map("title") %>% unlist(),
+         File = Slides %>% map("file") %>% unlist(),
+         Format = Slides %>% map("format") %>% unlist(),
+         PresentationTopic = Slides %>% map("topics")) %>%
+  select(-Slides) %>%
+  unnest(Presenter, .preserve = PresentationTopic) %>%
+  unnest(PresentationTopic, .preserve = Presenter) %>%
+  unnest()
 
 # join all the info together
-events <- dplyr::left_join(generalInfo, speakerInfo, by='ID') %>% 
-    dplyr::left_join(topicInfo, by='ID') %>% 
+events <- dplyr::left_join(generalInfo, speakerInfo, by='ID') %>%
+    dplyr::left_join(topicInfo, by='ID') %>%
     dplyr::left_join(presentationInfo, by='ID') %>% tibble::as_tibble()
 
 # make links
-events <- events %>% 
+events <- events %>%
     # create links to meetup pages
-    dplyr::mutate(EventMeetup=Meetup) %>% 
-    dplyr::mutate(Meetup=sprintf('<a href="%s" target="_blank">%s</a>', URL, Meetup)) %>% 
+    dplyr::mutate(EventMeetup=Meetup) %>%
+    dplyr::mutate(Meetup=sprintf('<a href="%s" target="_blank">%s</a>', URL, Meetup)) %>%
     # create links to files
     dplyr::mutate(Presentation=sprintf('<a href="https://nyhackr.blob.core.windows.net/presentations/%s" target="_blank">%s</a>',
                                 File, Title)) %>%
     # get rid of list() in VideoSource
-    dplyr::mutate(VideoSource=stringr::str_replace(VideoSource, 'list\\(\\)', '')) %>% 
-    dplyr::mutate(Video=dplyr::if_else(!is.na(Video) & Video != "", 
-                                       sprintf('<a href="%s" target="_blank">%s</a>', Video, VideoSource), 
+    dplyr::mutate(VideoSource=stringr::str_replace(VideoSource, 'list\\(\\)', '')) %>%
+    dplyr::mutate(Video=dplyr::if_else(!is.na(Video) & Video != "",
+                                       sprintf('<a href="%s" target="_blank">%s</a>', Video, VideoSource),
                                        NA_character_))
 
-talks <- events %>% 
-    dplyr::group_by(ID, Meetup, EventMeetup, Presentation, Title, Venue, Date, Description, File, Format, Video, VideoSource) %>% 
+talks <- events %>%
+    dplyr::group_by(ID, Meetup, EventMeetup, Presentation, Title, Venue, Date, Description, File, Format, Video, VideoSource) %>%
     dplyr::summarize(
-        PresentationTopics=makeCharVector(PresentationTopic), 
+        PresentationTopics=makeCharVector(PresentationTopic),
         Presenter=makeCharVector(Presenter),
         EventMeetupTopics=makeCharVector(c(Topic, PresentationTopic)),
-        Speakers=makeCharVector(Speaker)) %>% 
-    tidyr::unite(ShareKey, ID, EventMeetup, File, sep='_', remove=FALSE) %>% 
-    dplyr::ungroup() %>% 
+        Speakers=makeCharVector(Speaker)) %>%
+    tidyr::unite(ShareKey, ID, EventMeetup, File, sep='_', remove=FALSE) %>%
+    dplyr::ungroup() %>%
     dplyr::mutate(ID=as.character(ID))
 
-eventDetails <- talks %>% 
-    dplyr::select(ShareKey, ID, Meetup, EventMeetup, Venue, Date, Description, Speakers, EventMeetupTopics, Video, VideoSource) %>% 
-    dplyr::mutate(ShareKey=ID) %>% 
-    dplyr::group_by(ID) %>% 
-    dplyr::slice(1) %>% 
+eventDetails <- talks %>%
+    dplyr::select(ShareKey, ID, Meetup, EventMeetup, Venue, Date, Description, Speakers, EventMeetupTopics, Video, VideoSource) %>%
+    dplyr::mutate(ShareKey=ID) %>%
+    dplyr::group_by(ID) %>%
+    dplyr::slice(1) %>%
     dplyr::ungroup()
 
 ## create shared data
@@ -148,16 +152,16 @@ all_shared <- SharedData$new(data=talks, key=~ShareKey, group=groupMeetup)
 # a tibble of all columns for eventDetails
 details_shared <- SharedData$new(data=eventDetails, key=~ShareKey, group=groupMeetup)
 # the talks tibble
-talks_shared <- SharedData$new(data=talks %>% 
+talks_shared <- SharedData$new(data=talks %>%
                                    # drop rows where presenter is NA
                                    dplyr::filter(!is.na(Presenter)) %>%
                                    # keep these columns
-                                   dplyr::select(ShareKey, Date, Presentation, Meetup, Presenter), 
+                                   dplyr::select(ShareKey, Date, Presentation, Meetup, Presenter),
                                key=~ShareKey, group=groupMeetup)
 # events tibble
-events_shared <- SharedData$new(data=eventDetails %>% 
+events_shared <- SharedData$new(data=eventDetails %>%
                                     # keep these columns
-                                    dplyr::select(ShareKey, Date, Description, Meetup, Speakers, Video), 
+                                    dplyr::select(ShareKey, Date, Description, Meetup, Speakers, Video),
                                 key=~ShareKey, group=groupMeetup)
 
 ## filter objects
