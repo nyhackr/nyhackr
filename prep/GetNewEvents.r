d96467a46c12c825686fd926bebd61b4ebb18b72
Michael Beigelmacher <brooklynbagel@gmail.com>
2020-07-31 15:35:15 -0400

    Redone GetNewEvents using {meetupr}

--- prep/GetNewEvents.r
+++ prep/GetNewEvents.r
@@ -1,74 +1,54 @@
-library(httr)
+library(meetupr)
+library(dplyr)
 library(jsonlite)
-library(magrittr)
 library(purrr)
+library(tidyr)
+library(stringr)
 
-trimInfo <- function(info)
-{
-    # drop fields
-    info$created <- NULL
-    info$duration <- NULL
-    info$fee <- info$fee$amount
-    info$updated <- NULL
-    info$utc_offset <- NULL
-    info$venue$repinned <- NULL
-    info$venue$countrycode <- info$venue$country
-    info$venue$country <- info$venue$localized_country_name
-    info$venue$localized_country_name <- NULL
-    info$group$lat <- NULL
-    info$group$lon <- NULL
-    info$visibility <- NULL
-    info$group <- NULL
-    info$manual_attendance_count <- NULL
-    info$how_to_find_us <- NULL
-    
-    # add placeholders for manually edited fields
-    info$speakers <- character()
-    info$topics <- character()
-    info$slides <- character()
-    info$video <- character()
-    info$VideoSource <- character()
-    
-    return(info)
-}
 
-# make request from meetup API
-r <- GET('https://api.meetup.com/nyhackr/events', 
-         query=list(
-             status='past', 
-             desc='true', 
-             page='10'
-         )
-)
+old_events <- fromJSON(here::here("data/events.json"), simplifyDataFrame= FALSE)
+old_ids <- old_events %>%
+    map_chr("id")
 
-# parse the JSON response into a list and then trim the data
-meetupInfo <- content(r, as = 'parsed') %>% 
-    map(trimInfo)
+urlname <- "nyhackr"
+past_events <- get_events(urlname, "past")
 
-# read in previously stored events in data/events.json as a list
-oldEvents <- readLines('data/events.json') %>% 
-    fromJSON(simplifyDataFrame=FALSE)
+new_events <- past_events %>%
+    filter(!id %in% old_ids) %>%
+    hoist(
+        resource,
+        "rsvp_limit"
+    ) %>%
+    nest(venue = starts_with("venue")) %>%
+    select(
+        id,
+        name,
+        status,
+        time,
+        link,
+        description,
+        waitlist_count,
+        yes_rsvp_count,
+        rsvp_limit,
+        venue
+    ) %>%
+    mutate(
+        time = time * 1000,
+        speakers = list(list()),
+        topics = list(list()),
+        slides = list(list()),
+        venue = map(venue, function(x) {
+            x %>%
+                rename_all(str_remove, pattern = "^venue_") %>%
+                mutate(across(everything(), ~ replace_na(.x, ""))) %>%
+                as.list()
+        })
+    ) %>%
+    transpose()
 
-# find all event ids from the Meetup API response not found in the stored events
-newIds <- meetupInfo %>% 
-    map('id') %>% 
-    setdiff(oldEvents %>% map('id')) %>% 
-    unlist
+updated <- c(old_events, new_events)
+updated %>%
+    toJSON() %>%
+    prettify() %>%
+    writeLines(here::here("data/events.json"), useBytes=TRUE)
 
-# if there exist any new event ids, filter out the new events and append them
-# to the events data
-if (length(newIds) > 0)
-{
-    # extract new events by id, create appended data set and save to
-    # (temporary) data/events.json.new
-    new_events <- meetupInfo %>% 
-        keep(function(x) x[['id']] %in% newIds) %>% 
-        c(oldEvents, .) %>% 
-        toJSON() %>% 
-        prettify() %>% 
-        writeLines('data/events.json.new', useBytes=TRUE)
-    # save old data/events.json to data/events.json.old and move new
-    # data/events.json.new to data/events.json
-    file.rename('data/events.json', 'data/events.json.old')
-    file.rename('data/events.json.new', 'data/events.json')
-}
