9c7f5e9f5a010d5bd70226c7fc85a553bde13247
Joe Marlo <joe.marlo.09@gmail.com>
2022-07-07 12:07:11 -0500

    fix: only overwrite descriptionHTML column with meetup data if there is no content

--- R/data/update-data.R
+++ R/data/update-data.R
@@ -11,11 +11,19 @@ talks_current <- get_current_talks(gsheet_id)
 
 # only add new talks if their ID is not in the current data but also update
 # relevant data for old talks in the data
-# TODO: does this work when there is multiple presentations per event?
 cols_to_keep <- c('descriptionHTML', 'topics', 'videoURL', 'slidesTitle', 'slidesURL', 'speaker', 'cardURL', 'ticketsHTML')
 talks_to_add <- left_join(select(talks_to_add, -all_of(cols_to_keep)),
                        select(talks_current, ID, all_of(cols_to_keep)),
                        by = 'ID')
+
+# use descriptionHTML from meetup if its empty in the current data
+talks_to_add <- talks_to_add %>% 
+  rowwise() %>%
+  mutate(descriptionHTML = ifelse(
+    is.na(descriptionHTML), 
+    talks_upcoming$descriptionHTML[talks_upcoming$ID == ID],
+    descriptionHTML)) %>% 
+  ungroup()
 
 # make sure MeetUp link has utm_source tag
 if (nrow(talks_to_add) > 0){
